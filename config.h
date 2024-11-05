4f52b91035633b86b0ae9220ee476d9846074321
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-11-06 00:00:18 +1100

    added statuspadding

--- config.h
+++ config.h
@@ -15,6 +15,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 11;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "UbuntuMono Nerd Font:size=12" };
 static const char dmenufont[]       = "UbuntuMono Nerd Font:size=15";
 static const char col_gray1[]       = "#222222";
